6606c1199fecbfcf33abfc069c89da06b68e385c
root <root@ip-172-31-8-85.us-east-2.compute.internal>
2025-11-18 15:58:21 +0000

    Initial commit - Mini DevOps Project

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/hello", methods=["GET"])
+def hello():
+    return jsonify(message="Hello from DevOps – Kiran Garud")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
